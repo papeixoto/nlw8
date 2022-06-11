9ae1f326781f099328a9d602bea58ec98ee010d9
paulo-5mins <paulo@5mins.ai>
2022-06-11 19:04:08 +0100

    popover for feedback

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,5 @@
-function App() {
-  return <h1>yo</h1>;
-}
+import { Widget } from "./components/Widget";
 
-export default App;
+export function App() {
+  return <Widget />;
+}
---
+++ web/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          500: "#8257E6",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
